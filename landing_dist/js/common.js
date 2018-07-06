a53b7ac22c6f7d0089ea70618da9a03636966688
New resp <35687996+YevhenSoldatov1998@users.noreply.github.com>
2018-07-06 16:17:49 +0300

    Add files via upload

---
+++ landing_dist/js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+ $(".a_item").mPageScroll2id();
+
+	$(document).ready(function(){
+    $(".down").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 300);
+    });
+});
+
+});
